72bc2a4ee74ccff56d952b83346f63f5969ff1f1
Nathan Hartland <nathan@pseudorandom.dev>
2025-02-02 22:36:26 +0100

    Remove indicators

--- nvim/init.lua
+++ nvim/init.lua
@@ -14,13 +14,6 @@ vim.diagnostic.config({
     signs = true,
 })
 
---vim.api.nvim_create_autocmd("InsertEnter", {
---    callback = function() vim.cmd("hi Normal ctermbg=234 guibg=#000000") end,
---})
---vim.api.nvim_create_autocmd("InsertLeave", {
---    callback = function() vim.cmd("hi Normal ctermbg=232 guibg=#1b1d1e") end,
---})
-
 vim.opt.backup = true
 vim.opt.backupdir = { "~/.vim-tmp", "~/.tmp", "~/tmp", "/var/tmp", "/tmp" }
 vim.opt.backupskip = { "/tmp/*", "/private/tmp/*" }
