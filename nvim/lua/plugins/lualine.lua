1d93cdc83f4fe45671162f311abcf3f825c92c86
Nathan Hartland <nathan@pseudorandom.dev>
2025-02-12 13:25:41 +0100

    Updates

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -1,6 +1,9 @@
 return {
     "nvim-lualine/lualine.nvim",
-    dependencies = { "nvim-tree/nvim-web-devicons" },
+    dependencies = {
+        'nvim-lua/plenary.nvim',
+        "nvim-tree/nvim-web-devicons"
+    },
     opts = {
         theme = "auto",
 
