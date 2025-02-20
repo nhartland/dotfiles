cd9d0e303a375a6179e938a2b89afcb7e6d59ee2
Nathan Hartland <nathan@pseudorandom.dev>
2025-02-20 23:07:10 +0100

    Keymaps

--- nvim/lua/plugins/autocompletion.lua
+++ nvim/lua/plugins/autocompletion.lua
@@ -70,6 +70,7 @@ return {
                     --  This will auto-import if your LSP supports it.
                     --  This will expand snippets if the LSP sent a snippet.
                     ['<S-Tab>'] = cmp.mapping.confirm { select = true },
+                    ['<Right>'] = cmp.mapping.confirm { select = true },
 
                     -- If you prefer more traditional completion keymaps,
                     -- you can uncomment the following lines
