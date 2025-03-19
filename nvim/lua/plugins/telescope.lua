3c7578f3ae704153511bb526f4311855cddcc031
Nathan Hartland <nathan@pseudorandom.dev>
2025-03-19 14:41:02 +0100

    Telescope ui

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -10,56 +10,66 @@ local function telescope_with_root(picker)
 end
 
 return {
-    'nvim-telescope/telescope.nvim',
-    branch = '0.1.x',
-    dependencies = {
-        'nvim-lua/plenary.nvim',
-        { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
-        { "nvim-telescope/telescope-frecency.nvim",   version = "*" }
-    },
-    event = "VeryLazy",
-    opts = {
-        defaults = {
-            file_ignore_patterns = {
-                "%.pyc",
-                "%.wav", "%.ogg", "%.jfxr",
-                "%.png", "%.jpg", "%.jpeg", "%.bmp", "%.gif",
-                "%.zip",
-                "%.aif", "%.ttf",
-            }
+    {
+        'nvim-telescope/telescope.nvim',
+        branch = '0.1.x',
+        dependencies = {
+            'nvim-lua/plenary.nvim',
+            { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
+            { "nvim-telescope/telescope-frecency.nvim",   version = "*" },
+            { "nvim-telescope/telescope-ui-select.nvim",  version = "*" }
         },
-        pickers = {
+        event = "VeryLazy",
+        opts = {
             defaults = {
-                dynamic_preview_title = true,
-                path_display = { shorten = 3 },
-                theme = "ivy",
+                file_ignore_patterns = {
+                    "%.pyc",
+                    "%.wav", "%.ogg", "%.jfxr",
+                    "%.png", "%.jpg", "%.jpeg", "%.bmp", "%.gif",
+                    "%.zip",
+                    "%.aif", "%.ttf",
+                }
             },
-            find_files = {
-                prompt_title = false,  -- Removes the unnecessary title
-                prompt_prefix = "[Find Files] ",
-                results_title = false, -- Removes the unnecessary title
-                preview = { hide_on_startup = true },
-                theme = "ivy",
-                layout_config = { height = 0.30 },
+            extensions = {
+                ["ui-select"] = {
+                    require("telescope.themes").get_ivy {
+                        layout_config = { height = 0.30 },
+                    } }
             },
-            live_grep = {
-                dynamic_preview_title = true,
-                path_display = { shorten = 3 },
-            }
+            pickers = {
+                defaults = {
+                    dynamic_preview_title = true,
+                    path_display = { shorten = 3 },
+                    theme = "ivy",
+                },
+                find_files = {
+                    prompt_title = false,  -- Removes the unnecessary title
+                    prompt_prefix = "[Find Files] ",
+                    results_title = false, -- Removes the unnecessary title
+                    preview = { hide_on_startup = true },
+                    theme = "ivy",
+                    layout_config = { height = 0.30 },
+                },
+                live_grep = {
+                    dynamic_preview_title = true,
+                    path_display = { shorten = 3 },
+                }
+            },
         },
+        config = function(_, opts)
+            require("telescope").setup(opts)
+            require("telescope").load_extension("frecency")
+            require("telescope").load_extension("fzf")
+            require("telescope").load_extension("base16_colorpicker")
+            require("telescope").load_extension("ui-select")
+        end,
+        keys = {
+            { "<leader>fc", function() return require("telescope.builtin").find_files({ cwd = "~/dotfiles/" }) end, desc = "Telescope find config" },
+            { "<leader>ff", telescope_with_root("find_files"),                                                      desc = "Telescope find files" },
+            { "<leader>fg", telescope_with_root("live_grep"),                                                       desc = "Telescope live grep" },
+            { "<leader>fb", telescope_with_root("buffers"),                                                         desc = "Telescope buffers" },
+            { "<leader>fh", telescope_with_root("help_tags"),                                                       desc = "Telescope help tags" },
+            { "<leader>e",  function() return require("telescope").extensions.frecency.frecency() end,              desc = "Frecency-based file navigation" },
+        }
     },
-    config = function(_, opts)
-        require("telescope").setup(opts)
-        require("telescope").load_extension("frecency")
-        require("telescope").load_extension("fzf")
-        require("telescope").load_extension("base16_colorpicker")
-    end,
-    keys = {
-        { "<leader>fc", function() return require("telescope.builtin").find_files({ cwd = "~/dotfiles/" }) end, desc = "Telescope find config" },
-        { "<leader>ff", telescope_with_root("find_files"),                                                      desc = "Telescope find files" },
-        { "<leader>fg", telescope_with_root("live_grep"),                                                       desc = "Telescope live grep" },
-        { "<leader>fb", telescope_with_root("buffers"),                                                         desc = "Telescope buffers" },
-        { "<leader>fh", telescope_with_root("help_tags"),                                                       desc = "Telescope help tags" },
-        { "<leader>e",  function() return require("telescope").extensions.frecency.frecency() end,              desc = "Frecency-based file navigation" },
-    }
 }
