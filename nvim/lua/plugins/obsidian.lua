619d746f789ae19b7459b24e1ee95b8a33c53a8e
Nathan Hartland <nathan@pseudorandom.dev>
2026-02-14 00:33:34 +0100

    Obsidian

--- nvim/lua/plugins/obsidian.lua
+++ nvim/lua/plugins/obsidian.lua
@@ -63,28 +63,19 @@ return
         },
     },
     {
-        "epwalsh/obsidian.nvim",
-        version = "*", -- recommended, use latest release instead of latest commit
+        "obsidian-nvim/obsidian.nvim",
+        version = "*",
         lazy = true,
-        --ft = "markdown",
-        -- Replace the above line with this if you only want to load obsidian.nvim for markdown files in your vault:
-        cmd = { "ObsidianToday", "ObsidianTomorrow", "ObsidianYesterday", "ObsidianNew", "ObsidianSearch" },
+        cmd = { "Obsidian" },
         event = {
-            -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
-            -- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/*.md"
-            -- refer to `:h file-pattern` for more examples
             "BufReadPre " .. vim.fn.expand "~" .. "/vault/**/*.md",
             "BufNewFile " .. vim.fn.expand "~" .. "/vault/**/*.md",
         },
-        dependencies = {
-            -- Required.
-            "nvim-lua/plenary.nvim",
-        },
         keys = {
-            { "<leader>os", "<cmd>ObsidianSearch<cr>",    mode = "n", desc = "[O]bsidian [S]earch" },
-            { "<leader>on", "<cmd>ObsidianNew<cr>",       mode = "n", desc = "[O]bsidian [N]ew" },
-            { "<leader>ot", "<cmd>ObsidianToday<cr>",     mode = "n", desc = "[O]bsidian [T]oday" },
-            { "<leader>oy", "<cmd>ObsidianYesterday<cr>", mode = "n", desc = "[O]bsidian [Y]esterday" }
+            { "<leader>os", "<cmd>Obsidian search<cr>",    mode = "n", desc = "[O]bsidian [S]earch" },
+            { "<leader>on", "<cmd>Obsidian new<cr>",       mode = "n", desc = "[O]bsidian [N]ew" },
+            { "<leader>ot", "<cmd>Obsidian today<cr>",     mode = "n", desc = "[O]bsidian [T]oday" },
+            { "<leader>oy", "<cmd>Obsidian yesterday<cr>", mode = "n", desc = "[O]bsidian [Y]esterday" }
         },
         opts = {
             ui = {
@@ -99,111 +90,54 @@ return
             },
 
             daily_notes = {
-                -- Optional, if you keep daily notes in a separate directory.
                 folder = "daily",
-                -- Optional, if you want to change the date format for the ID of daily notes.
-                date_format = "%Y-%m-%d",
-                -- Optional, if you want to change the date format of the default alias of daily notes.
-                alias_format = "%B %-d, %Y",
-                -- Optional, default tags to add to each new daily note created.
+                date_format = "YYYY-MM-DD",
+                alias_format = "MMMM D, YYYY",
                 default_tags = { "daily-notes" },
             },
 
-            -- Optional, completion of wiki links, local markdown links, and tags using nvim-cmp.
-            completion = {
-                -- Set to false to disable completion.
-                nvim_cmp = false,
-                -- Trigger completion at 2 chars.
-                min_chars = 2,
+            note = {
+                template = "note.md",
             },
 
-            -- Optional, configure key mappings. These are the defaults. If you don't want to set any keymappings this
-            -- way then set 'mappings = {}'.
-            mappings = {
-                -- Overrides the 'gd' mapping to work on markdown/wiki links within your vault.
-                ["gd"] = {
-                    action = function()
-                        return require("obsidian").util.gf_passthrough()
-                    end,
-                    opts = { noremap = false, expr = true, buffer = true },
-                },
-                -- Smart action depending on context, either follow link or toggle checkbox.
-                ["<cr>"] = {
-                    action = function()
-                        return require("obsidian").util.smart_action()
-                    end,
-                    opts = { buffer = true, expr = true },
-                }
-            },
-
-            -- Optional, customize how note IDs are generated given an optional title.
-            ---@param title string|?
-            ---@return string
             note_id_func = function(title)
-                -- Create note IDs in a Zettelkasten format with a timestamp and a suffix.
-                -- In this case a note with the title 'My new note' will be given an ID that looks
-                -- like '1657296016-my-new-note', and therefore the file name '1657296016-my-new-note.md'
+                local date = os.date("%Y-%m-%d")
                 local suffix = ""
                 if title ~= nil then
-                    -- If title is given, transform it into valid file name.
                     suffix = title:gsub(" ", "-"):gsub("[^A-Za-z0-9-]", ""):lower()
                 else
-                    -- If title is nil, just add 4 random uppercase letters to the suffix.
                     for _ = 1, 4 do
                         suffix = suffix .. string.char(math.random(65, 90))
                     end
                 end
-                return tostring(os.time()) .. "-" .. suffix
+                return date .. "-" .. suffix
             end,
 
-            -- Where to put new notes. Valid options are
-            --  * "current_dir" - put new notes in same directory as the current buffer.
-            --  * "notes_subdir" - put new notes in the default notes subdirectory.
+            frontmatter = { enabled = true },
+
+            notes_subdir = "inbox",
             new_notes_location = "notes_subdir",
-            -- Either 'wiki' or 'markdown'.
             preferred_link_style = "markdown",
-            -- Optional, for templates (see below).
+
             templates = {
                 folder = "templates",
-                --date_format = "%Y-%m-%d",
-                date_format = "%-d %B, %Y",
-                time_format = "%H:%M",
-                -- A map for custom variables, the key should be the variable and the value a function
+                date_format = "D MMMM, YYYY",
+                time_format = "HH:mm",
                 substitutions = {},
             },
-            picker = {
-                -- Set your preferred picker. Can be one of 'telescope.nvim', 'fzf-lua', or 'mini.pick'.
-                name = "telescope.nvim",
-                -- Optional, configure key mappings for the picker. These are the defaults.
-                -- Not all pickers support all mappings.
-                note_mappings = {
-                    -- Create a new note from your query.
-                    new = "<C-x>",
-                    -- Insert a link to the selected note.
-                    insert_link = "<C-l>",
-                },
-                tag_mappings = {
-                    -- Add tag(s) to current note.
-                    tag_note = "<C-x>",
-                    -- Insert a tag at the current location.
-                    insert_tag = "<C-l>",
-                },
+
+            legacy_commands = false,
+
+            callbacks = {
+                enter_note = function()
+                    vim.keymap.set("n", "gd", "<cmd>Obsidian follow_link<cr>", { buffer = true })
+                    vim.keymap.set("n", "<cr>", "<cmd>Obsidian toggle_checkbox<cr>", { buffer = true })
+                end,
             },
 
-            -- Optional, sort search results by "path", "modified", "accessed", or "created".
-            -- The recommend value is "modified" and `true` for `sort_reversed`, which means, for example,
-            -- that `:ObsidianQuickSwitch` will show the notes sorted by latest modified time
-            sort_by = "modified",
-            sort_reversed = true,
-
-            -- Set the maximum number of lines to read from notes on disk when performing certain searches.
-            search_max_lines = 1000,
-
-            -- Optional, determines how certain commands open notes. The valid options are:
-            -- 1. "current" (the default) - to always open in the current window
-            -- 2. "vsplit" - to open in a vertical split if there's not already a vertical split
-            -- 3. "hsplit" - to open in a horizontal split if there's not already a horizontal split
-            open_notes_in = "current",
+            picker = {
+                name = "telescope.nvim",
+            },
         }
     }
 }
